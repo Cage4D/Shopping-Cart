328eb077ec64e8ca34da94435d653e38518d8b5d
Br0cod3 <thespammingbott@gmail.com>
2025-12-19 12:01:52 +0100

    feat: setup routing with react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./routes/HomePage.jsx";
+import ShoppingPage from "./routes/ShoppingPage.jsx";
+import Cart from "./routes/Cart.jsx";
+import HomeLanding from "./routes/HomeLanding.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} >
+          <Route index element={<HomeLanding/>} />
+          <Route path="shop" element={<ShoppingPage />} />
+          <Route path="cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
