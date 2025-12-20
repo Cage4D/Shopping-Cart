57ac34930bd0f89b1cf2e23cd8e14dade0fb5709
Br0cod3 <thespammingbott@gmail.com>
2025-12-20 17:48:23 +0100

    feat: Hover transition on Header, ref creation & React import

--- src/routes/HomeLanding.jsx
+++ src/routes/HomeLanding.jsx
@@ -1,7 +1,41 @@
+import React from "react";
+
 function HomeLanding() {
-    return(
-        <div>This is the home landing page!</div>
-    )
+  const headerRef = React.useRef();
+  const typedRef = React.useRef();
+
+  React.useEffect(() => {}, []);
+
+  return (
+    <>
+      <section>
+        <h1 ref={headerRef}>
+          Welcome to &nbsp;
+          <span
+            className="
+            italic
+            relative
+            after:content-['']
+            after:absolute
+            after:left-0
+            after:-bottom-0.5
+            after:h-0.5
+            after:w-0
+            after:bg-blue-500
+            after:transition-[width]
+            after:duration-300
+            after:ease-in-out
+            hover:after:w-full"
+          >
+            Shop.cart
+          </span>
+        </h1>
+        <p>
+          <span ref={typedRef}></span>
+        </p>
+      </section>
+    </>
+  );
 }
 
 export default HomeLanding;
