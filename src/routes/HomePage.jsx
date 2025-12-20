2d2e5fbacf83b7960b099c1df426cf4a6eb951c7
Br0cod3 <thespammingbott@gmail.com>
2025-12-21 00:26:03 +0100

    feat: Home renders NavBar and Outlet

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,13 @@
-function Home() {
+import NavBar from "../components/NavBar";
+import { NavLink, Link, Outlet } from "react-router";
 
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
 }
 
 export default Home;
