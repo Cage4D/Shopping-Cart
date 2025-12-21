5d986126b76e0ba08fe60ff7933246f8dcfe8e3e
Br0cod3 <thespammingbott@gmail.com>
2025-12-21 01:13:19 +0100

    feat: page routing functionality on section click

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router";
+
 function NavBar() {
-    return (
-        <nav className="flex justify-between pl-5 pr-8 py-2 items-center border-b-2 border-b-slate-200 h-[10vh]">
-            <p className="text-2xl font-bold font-clash tracking-tight">Shop<span className="text-slate-400">.cart</span></p>
-            <div className="flex items-center gap-8">
-                {["Home", "Shop", "Cart"].map(item => (
-                    <p
-                    key={item}
-                    className="relative
+  return (
+    <nav className="flex justify-between pl-5 pr-8 py-2 items-center border-b-2 border-b-slate-200 h-[10vh]">
+      <p className="text-2xl font-bold font-clash tracking-tight">
+        Shop<span className="text-slate-400">.cart</span>
+      </p>
+      <div className="flex items-center gap-8">
+        {["Home", "Shop", "Cart"].map((item) => (
+          <Link to={`${item === "Home" ? "/" : item }`}>
+            <p
+              key={item}
+              className="relative
                     font-clash font-medium text-slate-700
                     cursor-pointer
                     transition-colors duration-200
@@ -18,11 +23,15 @@ function NavBar() {
                     after:w-0
                     after:bg-slate-900
                     after:duration-200
-                    hover:after:w-full">{item}</p>
-                ))}
-            </div>
-        </nav>
-    )
+                    hover:after:w-full"
+            >
+              {item}
+            </p>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
