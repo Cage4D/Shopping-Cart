0436607311f8c9d38d8bb9d0f375da39f69c5167
Br0cod3 <thespammingbott@gmail.com>
2025-12-22 00:39:39 +0100

    feat: data fetching and rendering functionality

--- src/routes/ShoppingPage.jsx
+++ src/routes/ShoppingPage.jsx
@@ -1,5 +1,35 @@
-function ShoppingPage() {
+import React from "react";
+import ProductCard from "../components/ProductCard";
 
+function ShoppingPage() {
+  const [shopItems, setShopItems] = React.useState([]);
+  React.useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok)
+          throw new Error(`Response status: ${response.status}`);
+        const data = await response.json();
+        setShopItems(data);
+      } catch (e) {
+        console.error(e.message);
+      }
+    }
+    getData();
+  }, []);
+
+  return (
+    <section className="flex flex-wrap gap-5 justify-center py-5">
+        {shopItems.map(product => (
+            <ProductCard 
+            key={product.id}
+            image={product.image} 
+            title={product.title}
+            category={product.category}
+            price={product.price}/>
+        ))}
+    </section>
+  );
 }
 
 export default ShoppingPage;
